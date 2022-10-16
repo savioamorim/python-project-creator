89ae6f3d67fdb6265b649eb71f68e2bafde51de0
Savio Amorim <savioamorim.s@hotmail.com>
2022-10-16 11:35:07 -0300

    Updating script commands

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 import os
-import pip
 
 # Templates Path
 MAIN_TEMPLATE_PATH = "./templates/main.txt"
@@ -15,37 +14,42 @@ os.mkdir("./test")
 os.mkdir("./.vscode")
 
 # Creating main.py file
-main_template = open(MAIN_TEMPLATE_PATH, "r")
-file = open("./main.py", "w").write(main_template.read())
+main_template = open(MAIN_TEMPLATE_PATH, "r", encoding="utf-8")
+file = open("./main.py", "w", encoding="utf-8").write(main_template.read())
 
 # Creating .gitignore file
-gitignore_template = open(GIT_IGNORE_TEMPLATE_PATH, "r")
-file = open("./.gitignore", "w").write(gitignore_template.read())
+gitignore_template = open(GIT_IGNORE_TEMPLATE_PATH, "r", encoding="utf-8")
+file = open("./.gitignore", "w",
+            encoding="utf-8").write(gitignore_template.read())
 
 # Creating test.py file
-first_test_template = open(FIRST_TEST_TEMPLATE_PATH, "r")
-file = open("./test/test_template.py", "w").write(first_test_template.read())
+first_test_template = open(FIRST_TEST_TEMPLATE_PATH, "r", encoding="utf-8")
+file = open("./test/test_template.py", "w",
+            encoding="utf-8").write(first_test_template.read())
 
 # Creating launch.json file
-launch_template = open(LAUNCH_TEMPLATE_PATH, "r")
-file = open("./.vscode/launch.json", "w").write(launch_template.read())
+launch_template = open(LAUNCH_TEMPLATE_PATH, "r", encoding="utf-8")
+file = open("./.vscode/launch.json", "w",
+            encoding="utf-8").write(launch_template.read())
 
 # Creating settings.json file
-settings_template = open(SETTINGS_TEMPLATE_PATH, "r")
-file = open("./.vscode/settings.json", "w").write(settings_template.read())
+settings_template = open(SETTINGS_TEMPLATE_PATH, "r", encoding="utf-8")
+file = open("./.vscode/settings.json", "w",
+            encoding="utf-8").write(settings_template.read())
 
 # Creating first_class.py file
-first_class_template = open(FIRST_CLASS_TEMPLATE_PATH, "r")
-file = open("./src/my_first_class.py", "w").write(first_class_template.read())
+first_class_template = open(FIRST_CLASS_TEMPLATE_PATH, "r", encoding="utf-8")
+file = open("./src/my_first_class.py", "w",
+            encoding="utf-8").write(first_class_template.read())
 
 # Creating python environment
-os.system("python -m venv venv")
+os.system("python3 -m venv venv")
 
 # TODO Activating python environment
 
 # Updating pip
-pip.main(["install", "--upgrade", "pip"])
+os.system('./venv/bin/python3 -m pip install --upgrade pip')
 
 # Installing basic dependencies
-pip.main(["install", "-U", "autopep8"])
-pip.main(["install", "-U", "pylint"])
+os.system('./venv/bin/python3 -m pip install -U autopep8')
+os.system('./venv/bin/python3 -m pip install -U pylint')
